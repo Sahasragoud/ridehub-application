86100b10f9cfb124fc09a8f0e3b0966a52c5ef79
Sahasragoud <sahasramuthyala04@gmail.com>
2026-08-16 12:09:45 +0530

    no more

--- RideHub-Auth-Service/src/main/java/com/rideHub/authService/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.rideHub.authService.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
